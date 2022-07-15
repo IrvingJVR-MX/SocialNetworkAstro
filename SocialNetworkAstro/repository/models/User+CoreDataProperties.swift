aa7f45026d3fde4a109b9cbb6b9e56b2977d90fc
Irving J Villanueva Reyes <Irving.Villanueva@jala-foundation.org>
2022-07-15 00:07:38 -0500

    Added files for core data

---
+++ SocialNetworkAstro/repository/models/User+CoreDataProperties.swift
@@ -0,0 +1,17 @@
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var userid: String?
+
+}
+
+extension User : Identifiable {
+
+}
